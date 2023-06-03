6eb8a6e61ded9e6568ef96cecff6431f7b51abfe
Renato Lucena <cpdrenato@gmail.com>
2023-06-03 14:19:10 -0300

    adicionado no produto controller o index que busca all()

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -2,11 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\ProdutoRepository;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Exception;
 
 class ProdutoController extends Controller
 {
-    //
+    protected $produtoRepository;
+
+    public function __construct(ProdutoRepository $produtoRepository)
+    {
+        $this->produtoRepository = $produtoRepository;
+    }
+
+    public function index()
+    {
+        try {
+            return response()->json($this->produtoRepository->all(), Response::HTTP_OK);
+        } catch (Exception $error) {
+
+        }
+    }
 }
